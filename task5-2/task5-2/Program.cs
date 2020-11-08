9d9c4a029dd65b3522c6b73dbdf817c176002765
Анастасия Щукина <a.shchukina03@inbox.ru>
2020-11-08 22:56:06 +0500

    Исправлена ошибка в 5 задании

---
+++ task5-2/task5-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word1 = "кинематограф";
+
+            string word2 = word1.Substring(8, 4) + word1[7] + word1.Substring(4, 2) + word1[2];
+
+            Console.WriteLine("Первое полученное слово:" + word2);
+
+            string word3 = word1.Substring(4, 1) + word1[3] + word1[6] + word1[9] + word1[7];
+            Console.WriteLine("Второе полученное слово:" + word3);
+
+            Console.ReadKey();
+        }
+    }
+}
