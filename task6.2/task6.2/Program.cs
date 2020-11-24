aef92c22e2f55a647773e76bdc99ba75166412f3
Анастасия Щукина <a.shchukina03@inbox.ru>
2020-11-24 20:31:14 +0500

    Выполнено задание 6.2

---
+++ task6.2/task6.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координату x");
+            var x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите координату y");
+            var y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Принадлежит ли точка области? " + IsPointInArea(x, y));
+
+            Console.ReadKey();
+        }
+
+        static bool IsPointInArea(double x, double y)
+        {
+            return x <= -3 && -2 <= y ||
+                -1 <= x && -2 <= y;
+        }
+    }
+}
