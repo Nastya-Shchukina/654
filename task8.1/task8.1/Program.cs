9997e107219ab797578928c575fd10ce9254e405
Анастасия Щукина <a.shchukina03@inbox.ru>
2020-12-27 17:35:34 +0500

    Исправлена ошибка

---
+++ task8.1/task8.1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i = 1, sum = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Введите число: ");
+
+                if (!int.TryParse(Console.ReadLine(), out n))
+                {
+                    Console.WriteLine("Ошибка ввода\n");
+                    continue;
+                }
+                else
+                {
+                    int result = CountSum(n);
+                    Console.WriteLine($"Полученное число:" + result);
+                }
+                break;
+               
+            }
+
+        }
+
+        static int CountSum(int n)
+        {
+            int i = 1, sum = 0, result = 0;
+            do
+            {
+                sum += i * i;
+                result = result + sum;
+                i++;
+
+            } while (sum > n);
+
+            return result;
+        }
+    }
+}
