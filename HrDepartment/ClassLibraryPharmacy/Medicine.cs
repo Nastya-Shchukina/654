b6c923b0e596b4476eae47bcf310f15eba1bd419
Анастасия Щукина <a.shchukina03@inbox.ru>
2021-03-28 15:28:43 +0500

    14 вариант

---
+++ HrDepartment/ClassLibraryPharmacy/Medicine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryPharmacy
+{
+    public class Medicine
+    {
+        public readonly string Articul;
+        public string Name;
+        public MedicineRecept Recept;
+        public string Maker;
+        public double Price;
+        public int Availibility;
+
+        public Medicine(string articul, string name, MedicineRecept recept, string maker)
+        {
+            Articul = articul;
+            Name = name;
+            Recept = recept;
+            Maker = maker;
+        }
+
+        public override string ToString()
+        {
+            return $"{Articul} {Name}";
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine(this);
+
+            var recept = "";
+            switch (Recept)
+            {
+                case MedicineRecept.Yes:
+                    recept = "Отпуск по рецепту";
+                    break;
+                case MedicineRecept.No:
+                    recept = "Отпуск без рецепта";
+                    break;
+            }
+
+            Console.WriteLine($"Производитель: {Maker}. Цена: {Price}. Количество на складе: {Availibility}.");
+        }
+    }
+}
---
+++ HrDepartment/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryPharmacy;
+using System.IO;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ConstructorTestMethod()
+        {
+            var paracetomol = CreateTestMedicine();
+
+            Assert.AreEqual("X372L", paracetomol.Articul);
+            Assert.AreEqual("Paracetomol", paracetomol.Name);
+            Assert.AreEqual(MedicineRecept.No, paracetomol.Recept);
+            Assert.AreEqual("Renewal", paracetomol.Maker);
+        }
+
+        [TestMethod]
+        public void ToStringTestMethod()
+        {
+            var paracetomol = CreateTestMedicine();
+            Assert.AreEqual("X372L Paracetomol", paracetomol.ToString());
+        }
+
+        [TestMethod]
+        public void PrintInfoTestMethod()
+        {
+            var paracetomol = CreateTestMedicine();
+            var omega = new Medicine("D993P", "Omega", MedicineRecept.Yes, "Solgar");
+
+            var consoleOut = new[]
+            {
+"X372L Paracetomol",
+$"Производитель: Renewal. Цена: 31. Количество на складе: 238.",
+"D993P Omega",
+$"Производитель: Solgar. Цена: 1200. Количество на складе: 24."
+};
+
+            paracetomol.Price = 31;
+            paracetomol.Availibility = 238;
+            omega.Price = 1200;
+            omega.Availibility = 24;
+
+            TextWriter oldOut = Console.Out;
+            StringWriter output = new StringWriter();
+            Console.SetOut(output);
+
+            paracetomol.PrintInfo();
+            omega.PrintInfo();
+
+            Console.SetOut(oldOut);
+
+            var outputArray = output.ToString().Split(new[] { "\r\n" },
+            StringSplitOptions.RemoveEmptyEntries);
+
+            Assert.AreEqual(4, outputArray.Length);
+            for (var i = 0; i < consoleOut.Length; i++)
+                Assert.AreEqual(consoleOut[i], outputArray[i]);
+        }
+
+        private Medicine CreateTestMedicine()
+        {
+            return new Medicine("X372L", "Paracetomol", MedicineRecept.No, "Renewal");
+        }
+
+    }
+}
