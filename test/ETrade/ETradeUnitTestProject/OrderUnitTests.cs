a4a37fa23c4b582286bc126b2bdf92be5c409d58
shuk-nst <a.shchukina03@inbox.ru>
2021-12-28 13:46:03 +0500

    12 вариант, сделаны первые три пункта

---
+++ test/ETrade/ETradeUnitTestProject/OrderUnitTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ETrade;
+
+namespace ETradeUnitTestProject
+{
+    [TestClass]
+    public class OrderUnitTests
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            var comodity = new Comodity("Телевизор", "Samsung UE55TU7097U", 39990);
+            var order = new Order(1, comodity, 15);
+            Assert.AreEqual("Телевизор", order.Goods.Category);
+            Assert.AreEqual("Samsung UE55TU7097U", order.Goods.Name);
+            Assert.AreEqual(15, order.Quantity);
+        }
+
+        [TestMethod]
+        public void OrderCreatorTest()
+        {
+            var creator = new OrderCreator();
+
+            var order = creator.Create(1, "Телевизор\tSamsung UE55TU7097U\t39990\t15");
+            Assert.AreEqual(1, order.ID);
+            Assert.AreEqual("Телевизор", order.Goods.Category);
+            Assert.AreEqual("Samsung UE55TU7097U", order.Goods.Name);
+            Assert.AreEqual(15, order.Quantity);
+        }
+    }
+}
---
+++ test/ETrade/ETradeUnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ETradeUnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ETradeUnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3380cce7-ac39-433e-9d26-18fe6b6a5b16")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
